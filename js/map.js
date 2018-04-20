8aec30fc8556c502fa8798ed3b5bc9b84e37cd29
Idris Fagbemi <idrisniyi94@gmail.com>
2018-04-20 10:21:23 -0500

    fixing the javascript and adding some elements

--- js/map.js
+++ js/map.js
@@ -1,11 +1,20 @@
 function initMap() {
 	var uluru = {lat: 42.02142, lng: -87.6645018};
+	var loc1 = {lat: 41.8336417, lng: -87.6259347};
+  var loc2 = {lat: 42.0012436, lng: -87.6624455};
+  var loc3 = {lat: 40.9434769, lng: -90.3723725};
+  var loc4 = {lat: 41.9231283, lng: -87.6564171};
+
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
 		center: uluru
 	});
 	var marker = new google.maps.Marker({
-		position: uluru,
+		position: (uluru, loc1, loc2, loc3, loc4),
 		map: map
 	});
+}
+function goto(location){
+	map.panTo(location);
+	map.setzoom(15);	
 }
